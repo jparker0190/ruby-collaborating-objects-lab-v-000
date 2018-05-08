6c98efc9ccbd43930b432e737db43bddccac0e2c
Joseph Parker <jparker010190@gmail.com>
2018-05-08 00:21:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name 
   def initialize(name)
     @name = name
+  end
+
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
   end
 end

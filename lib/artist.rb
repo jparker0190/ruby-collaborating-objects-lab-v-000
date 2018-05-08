7e979e2c99f8f84c64087c2e56d7387ec58b42d9
Joseph Parker <jparker010190@gmail.com>
2018-05-08 00:32:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,4 +4,14 @@ class Artist
     @name = name
     @songs
   end
+
+  def self.find_or_create_by_name(name)
+    @@all.each do |artist|
+      if artist.name == name
+        return artist
+      else
+        return self.new(name)
+      end
+    end
+  end
 end

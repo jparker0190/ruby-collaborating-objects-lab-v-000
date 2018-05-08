31dbf3bf790c6f9ef5e99680c726e6212f624ad2
Joseph Parker <jparker010190@gmail.com>
2018-05-08 00:38:09 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,8 +6,6 @@ class MP3Importer
   end
 
   def import(list_of_filename)
-    list_of_filename.each do |filename|
-      Song.new_by_filename(filename)
-    end
+  files.each{|f| Song.new_by_filename(f)}
   end
 end

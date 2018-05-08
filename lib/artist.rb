f7b84a86754ae9bc15b4723785a13ba05b4b96ff
Joseph Parker <jparker010190@gmail.com>
2018-05-08 01:08:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,36 +7,26 @@ class Artist
     @songs = []
   end
 
-  def self.all
-    @@all
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    song
   end
 
   def songs
     @songs
   end
 
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
-  end
-
-  def self.find(name)
-    self.all.find {|artist| artist.name == name }
-  end
-
-
-  def self.create(name)
-    self.new(name).tap {|artist| artist.save}
-  end
-
   def save
     @@all << self
+    self
   end
 
-  def add_song(song)
-		@songs << song
-	end
+  def self.all 
+    @@all
+  end
 
-  def print_songs
-    @songs.each {|song| puts song.name}
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
   end
 end

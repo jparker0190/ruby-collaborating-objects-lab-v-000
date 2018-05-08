7baf9acc6e32b961c6f7752cfafcfddb329d55da
Joseph Parker <jparker010190@gmail.com>
2018-05-08 00:46:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,12 +5,8 @@ class Song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
+     self.artist = Artist.find_or_create_by_name(name)
+   end
 
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
